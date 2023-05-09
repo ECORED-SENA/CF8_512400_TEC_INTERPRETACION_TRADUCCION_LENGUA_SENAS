bd908c4d18a1c3368341b56e32d2dca101e24b6c
flizcano <flizcanor@icloud.com>
2023-05-08 21:17:58 -0500

    se adjunta descargable pdf

--- src/config/global.js
+++ src/config/global.js
@@ -88,6 +88,11 @@ export default {
         nombreRuta: 'referencias',
       },
       {
+        icono: 'fas fa-file-pdf',
+        titulo: 'Descargar PDF',
+        download: 'downloads/512400_CF08_DU.pdf',
+      },
+      {
         icono: 'fas fa-download',
         titulo: 'Descargar material',
         download: 'downloads/material.zip',
